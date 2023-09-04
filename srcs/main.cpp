e2390b8c20ea63e1b2112c333e0f6db17e69c858
NEIL-smtg <sunghui2000@hotmail.com>
2023-09-05 04:28:43 +0800

    testing

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: suchua <suchua@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: suchua <suchua@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 20:47:59 by suchua            #+#    #+#             */
-/*   Updated: 2023/08/28 20:49:07 by suchua           ###   ########.fr       */
+/*   Updated: 2023/09/05 04:25:41 by suchua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int	main(int ac, char **av)
 		std::cerr << "Error : Incorrect number of arguments\n";
 		return (1);
 	}
+	std::cout << av[1] << std::endl;
 	return (0);
 }
